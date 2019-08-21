a1f699ce035888b485f7404703bd8d3f7bb22e8d
juwillia <juwillia@cern.ch>
2019-08-21 20:11:02 +0200

    Split SimpleFlatTableProducer into header and plugin

--- PhysicsTools/NanoAOD/plugins/SimpleFlatTableProducerPlugins.cc
+++ PhysicsTools/NanoAOD/plugins/SimpleFlatTableProducerPlugins.cc
@@ -6,10 +6,14 @@ typedef SimpleFlatTableProducer<reco::Candidate> SimpleCandidateFlatTableProduce
 #include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
 typedef EventSingletonSimpleFlatTableProducer<GenEventInfoProduct> SimpleGenEventFlatTableProducer;
 
-#include "SimDataFormats/HTXS/interface/HiggsTemplateCrossSections.h"
-typedef EventSingletonSimpleFlatTableProducer<HTXS::HiggsClassification> SimpleHTXSFlatTableProducer;
+#include "DataFormats/ProtonReco/interface/ForwardProton.h"
+typedef SimpleFlatTableProducer<reco::ForwardProton> SimpleProtonTrackFlatTableProducer;
+
+#include "DataFormats/CTPPSReco/interface/CTPPSLocalTrackLite.h"
+typedef SimpleFlatTableProducer<CTPPSLocalTrackLite> SimpleLocalTrackFlatTableProducer;
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 DEFINE_FWK_MODULE(SimpleCandidateFlatTableProducer);
 DEFINE_FWK_MODULE(SimpleGenEventFlatTableProducer);
-DEFINE_FWK_MODULE(SimpleHTXSFlatTableProducer);
+DEFINE_FWK_MODULE(SimpleProtonTrackFlatTableProducer);
+DEFINE_FWK_MODULE(SimpleLocalTrackFlatTableProducer);
