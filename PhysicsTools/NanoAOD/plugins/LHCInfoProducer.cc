0d94d3e257bb36828a870853c7e153acd81c969d
Justin Williams <justinwilliams@ku.edu>
2020-05-29 13:17:09 -0500

    Added beam energy to LHCInfo table

--- PhysicsTools/NanoAOD/plugins/LHCInfoProducer.cc
+++ PhysicsTools/NanoAOD/plugins/LHCInfoProducer.cc
@@ -67,7 +67,7 @@ public:
     iSetup.get<LHCInfoRcd>().get(lhcInfo);
 
     std::unique_ptr<nanoaod::FlatTable> out = fillTable(iEvent, lhcInfo);
-    out->setDoc("LHC crossing angle");
+    out->setDoc("LHC Info");
 
     iEvent.put(std::move(out),"lhcInfoTab");
 
@@ -78,10 +78,12 @@ public:
     const LHCInfo* info = prod.product();
     float xangle = info->crossingAngle();
     float betaStar = info->betaStar();
+    float beamEnergy = info->energy();
 
     auto out = std::make_unique<nanoaod::FlatTable>(1,"LHCInfo",true);
     out->addColumnValue<float>("xangle", xangle, "LHC crossing angle", nanoaod::FlatTable::FloatColumn,precision_);
     out->addColumnValue<float>("betaStar",betaStar,"LHC beta star", nanoaod::FlatTable::FloatColumn,precision_);
+    out->addColumnValue<float>("beamEnergy",beamEnergy,"LHC beam energy", nanoaod::FlatTable::FloatColumn,precision_);
     return out;
   }
 
