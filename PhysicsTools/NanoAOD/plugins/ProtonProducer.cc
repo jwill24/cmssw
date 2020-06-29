0843e7c8ca3570766b74b320238ef256054854c8
Justin Williams <justinwilliams@ku.edu>
2020-06-29 11:05:51 -0500

    Added Jan's changes

--- PhysicsTools/NanoAOD/plugins/ProtonProducer.cc
+++ PhysicsTools/NanoAOD/plugins/ProtonProducer.cc
@@ -72,114 +72,100 @@ public:
     edm::Handle<std::vector<CTPPSLocalTrackLite> > ppsTracksLite;
     iEvent.getByToken( tokenTracksLite_, ppsTracksLite );
 
-    std::vector<int> protonRPId, protonRPType;
+    // book output variables for protons
+    std::vector<int> singleRP_RPId, singleRP_RPType;
     std::vector<bool> singleRP_sector45, singleRP_sector56, multiRP_sector45, multiRP_sector56;
+
+    // book output variables for tracks
     std::vector<float> trackX, trackXUnc, trackY, trackYUnc, trackTime, trackTimeUnc, localSlopeX, localSlopeY, normalizedChi2;
     std::vector<int> singleRPProtonIdx, multiRPProtonIdx, decRPId, numFitPoints, pixelRecoInfo, rpType;
-    int detId, subdetId;
-    bool multiRP_proton = false;
 
-    for (const auto &handle : {hRecoProtonsSingleRP, hRecoProtonsMultiRP}) {
-
-      int num_proton = handle->size();
-      int proton_pos = 0; // indexing convention for cross-linking
-      protonRPId.reserve( num_proton );
+    // process single-RP protons
+    {
+      const auto &num_proton = hRecoProtonsSingleRP->size();
+      singleRP_RPId.reserve( num_proton );
+      singleRP_RPType.reserve( num_proton );
       singleRP_sector45.reserve( num_proton );
       singleRP_sector56.reserve( num_proton );
-      multiRP_sector45.reserve( num_proton );
-      multiRP_sector56.reserve( num_proton );
-      
-      for (const auto &proton : *handle) {
-	int method = (proton.method() == reco::ForwardProton::ReconstructionMethod::singleRP) ? 0 : 1;
 
-	if (method == 0) { // singleRP protons
-	  CTPPSDetId rpId((*proton.contributingLocalTracks().begin())->getRPId());
-	  detId = (rpId.arm() * 100 + rpId.station() * 10 + rpId.rp() );
-	  subdetId = rpId.subdetId();
-	  protonRPId.push_back( detId );
-	  protonRPType.push_back( rpId.subdetId() );
-	  singleRP_sector45.push_back( (proton.pz() > 0.) ? true : false );
-	  singleRP_sector56.push_back( (proton.pz() < 0.) ? true : false );
-	} 
-	else if (method == 1) { //multiRP protons
-	  if (!multiRP_proton) multiRPProtonIdx = std::vector<int>(trackX.size(),-1);
-	  multiRP_proton = true;
-	  multiRP_sector45.push_back( (proton.pz() > 0.) ? true : false );
-	  multiRP_sector56.push_back( (proton.pz() < 0.) ? true : false );
-	}
-      
-	for (const auto& tr : proton.contributingLocalTracks()) { // PPSLocalTracks
-	  if (method == 0) {
-	    singleRPProtonIdx.push_back( proton_pos );
-	    trackX.push_back( tr->getX() );
-	    trackXUnc.push_back( tr->getXUnc() );
-	    trackY.push_back( tr->getY() );
-	    trackYUnc.push_back( tr->getYUnc() );
-	    trackTime.push_back( tr->getTime() );
-	    trackTimeUnc.push_back( tr->getTimeUnc() );
-	    decRPId.push_back( detId );
-	    numFitPoints.push_back( tr->getNumberOfPointsUsedForFit() );
-	    pixelRecoInfo.push_back( static_cast<int>(tr->getPixelTrackRecoInfo()) );
-	    normalizedChi2.push_back( tr->getChiSquaredOverNDF() );
-	    rpType.push_back( subdetId );
-	    localSlopeX.push_back( tr->getTx() );
-	    localSlopeY.push_back( tr->getTy() );
-	  } else if (method == 1) {
-	    auto it = std::find(trackX.begin(), trackX.end(), tr->getX() );
-	    int index = std::distance(trackX.begin(), it);
-	    multiRPProtonIdx[index] = proton_pos;
-	  }
-	} 
-	proton_pos++;
-      } 
-    }
-
-    while ( multiRPProtonIdx.size() < singleRPProtonIdx.size() ) multiRPProtonIdx.push_back(-1); // fill empy multiRP indices with -1
-
-    for ( const auto& trk : *ppsTracksLite ) { // Store PPS timing tracks
-      CTPPSDetId rpId(trk.getRPId());
-      int detId = (rpId.arm() * 100 + rpId.station() * 10 + rpId.rp() );
-      if (detId == 16 or detId == 116) {
-	singleRPProtonIdx.push_back(-1);
-	multiRPProtonIdx.push_back(-1);
-	trackX.push_back( trk.getX() );
-	trackXUnc.push_back( trk.getXUnc() );
-	trackY.push_back( trk.getY() );
-	trackYUnc.push_back( trk.getYUnc() );
-	trackTime.push_back( trk.getTime() );
-	trackTimeUnc.push_back( trk.getTimeUnc() );
-	decRPId.push_back( rpId.arm() * 100 + rpId.station() * 10 + rpId.rp() );
-	numFitPoints.push_back( trk.getNumberOfPointsUsedForFit() );
-	pixelRecoInfo.push_back( static_cast<int>(trk.getPixelTrackRecoInfo()) );
-	normalizedChi2.push_back( trk.getChiSquaredOverNDF() );
-	rpType.push_back( rpId.subdetId() );
-	localSlopeX.push_back( trk.getTx() );
-	localSlopeY.push_back( trk.getTy() );
+      for (const auto &proton : *hRecoProtonsSingleRP)
+      {
+        CTPPSDetId rpId((*proton.contributingLocalTracks().begin())->getRPId());
+        unsigned int rpDecId = rpId.arm() * 100 + rpId.station() * 10 + rpId.rp();
+        singleRP_RPId.push_back(rpDecId);
+        singleRP_RPType.push_back(rpId.subdetId());
+        singleRP_sector45.push_back( (proton.pz() > 0.) ? true : false );
+        singleRP_sector56.push_back( (proton.pz() < 0.) ? true : false );
       }
     }
 
+    // process multi-RP protons
+    {
+      const auto &num_proton = hRecoProtonsMultiRP->size();
+      multiRP_sector45.reserve( num_proton );
+      multiRP_sector56.reserve( num_proton );
 
-    auto ppsTab = std::make_unique<nanoaod::FlatTable>(trackX.size(),"PPSLocalTrack",false);
-    ppsTab->addColumn<int>("singleRPProtonIdx",singleRPProtonIdx,"local track - proton correspondence",nanoaod::FlatTable::IntColumn);
-    ppsTab->addColumn<int>("multiRPProtonIdx",multiRPProtonIdx,"local track - proton correspondence",nanoaod::FlatTable::IntColumn);
-    ppsTab->addColumn<float>("x",trackX,"local track x",nanoaod::FlatTable::FloatColumn,precision_);
-    ppsTab->addColumn<float>("xUnc",trackXUnc,"local track x uncertainty",nanoaod::FlatTable::FloatColumn,precision_);
-    ppsTab->addColumn<float>("y",trackY,"local track y",nanoaod::FlatTable::FloatColumn,precision_);
-    ppsTab->addColumn<float>("yUnc",trackYUnc,"local track y uncertainty",nanoaod::FlatTable::FloatColumn,precision_);
-    ppsTab->addColumn<float>("time",trackTime,"local track time",nanoaod::FlatTable::FloatColumn,precision_);
-    ppsTab->addColumn<float>("timeUnc",trackTimeUnc,"local track time uncertainty",nanoaod::FlatTable::FloatColumn,precision_);
-    ppsTab->addColumn<int>("decRPId",decRPId,"local track detector dec id",nanoaod::FlatTable::IntColumn);
-    ppsTab->addColumn<int>("numFitPoints",numFitPoints,"number of points used for fit",nanoaod::FlatTable::IntColumn);
-    ppsTab->addColumn<int>("pixelRecoInfo",pixelRecoInfo,"flag if a ROC was shifted by a bunchx",nanoaod::FlatTable::IntColumn);
-    ppsTab->addColumn<float>("normalizedChi2",normalizedChi2,"chi2 over NDF",nanoaod::FlatTable::FloatColumn,precision_);
-    ppsTab->addColumn<int>("rpType",rpType,"strip=3, pixel=4, diamond=5, timing=6",nanoaod::FlatTable::IntColumn);
-    ppsTab->addColumn<float>("localSlopeX",localSlopeX,"track horizontal angle",nanoaod::FlatTable::FloatColumn,precision_);
-    ppsTab->addColumn<float>("localSlopeY",localSlopeY,"track vertical angle",nanoaod::FlatTable::FloatColumn,precision_);
-    ppsTab->setDoc("ppsLocalTrack variables");
-    
+      for (const auto &proton : *hRecoProtonsMultiRP)
+      {
+          multiRP_sector45.push_back( (proton.pz() > 0.) ? true : false );
+          multiRP_sector56.push_back( (proton.pz() < 0.) ? true : false );
+      }
+    }
+
+    // process local tracks
+    for (unsigned int tr_idx = 0; tr_idx < ppsTracksLite->size(); ++tr_idx)
+    {
+      const auto& tr = ppsTracksLite->at(tr_idx);
+
+      CTPPSDetId rpId(tr.getRPId());
+      unsigned int rpDecId = rpId.arm() * 100 + rpId.station() * 10 + rpId.rp();
+
+	  decRPId.push_back(rpDecId);
+	  rpType.push_back(rpId.subdetId());
+      
+	  trackX.push_back( tr.getX() );
+	  trackXUnc.push_back( tr.getXUnc() );
+	  trackY.push_back( tr.getY() );
+	  trackYUnc.push_back( tr.getYUnc() );
+	  trackTime.push_back( tr.getTime() );
+	  trackTimeUnc.push_back( tr.getTimeUnc() );
+	  numFitPoints.push_back( tr.getNumberOfPointsUsedForFit() );
+	  pixelRecoInfo.push_back( static_cast<int>(tr.getPixelTrackRecoInfo()) );
+	  normalizedChi2.push_back( tr.getChiSquaredOverNDF() );
+	  localSlopeX.push_back( tr.getTx() );
+	  localSlopeY.push_back( tr.getTy() );
+
+      signed int singleRP_idx = -1;
+      for (unsigned int p_idx = 0; p_idx < hRecoProtonsSingleRP->size(); ++p_idx)
+      {
+        const auto &proton = hRecoProtonsSingleRP->at(p_idx);
+
+        for (const auto &ref : proton.contributingLocalTracks())
+        {
+          if (ref.key() == tr_idx)
+            singleRP_idx = p_idx;
+        }
+      }
+	  singleRPProtonIdx.push_back(singleRP_idx);
+
+      signed int multiRP_idx = -1;
+      for (unsigned int p_idx = 0; p_idx < hRecoProtonsMultiRP->size(); ++p_idx)
+      {
+        const auto &proton = hRecoProtonsMultiRP->at(p_idx);
+
+        for (const auto &ref : proton.contributingLocalTracks())
+        {
+          if (ref.key() == tr_idx)
+            multiRP_idx = p_idx;
+        }
+      }
+	  multiRPProtonIdx.push_back(multiRP_idx);
+    }
+
+    // update proton tables
     std::unique_ptr<edm::ValueMap<int>> protonRPIdV(new edm::ValueMap<int>());
     edm::ValueMap<int>::Filler fillerID(*protonRPIdV);
-    fillerID.insert(hRecoProtonsSingleRP, protonRPId.begin(), protonRPId.end());
+    fillerID.insert(hRecoProtonsSingleRP, singleRP_RPId.begin(), singleRP_RPId.end());
     fillerID.fill();
 
     std::unique_ptr<edm::ValueMap<bool>> singleRP_sector45V(new edm::ValueMap<bool>());
@@ -201,14 +187,34 @@ public:
     edm::ValueMap<bool>::Filler fillermulti56(*multiRP_sector56V);
     fillermulti56.insert(hRecoProtonsMultiRP, multiRP_sector56.begin(), multiRP_sector56.end());
     fillermulti56.fill();
-
+    
+    // build track table
+    auto ppsTab = std::make_unique<nanoaod::FlatTable>(trackX.size(), "PPSLocalTrack", false);
+    ppsTab->addColumn<int>("singleRPProtonIdx",singleRPProtonIdx,"local track - proton correspondence",nanoaod::FlatTable::IntColumn);
+    ppsTab->addColumn<int>("multiRPProtonIdx",multiRPProtonIdx,"local track - proton correspondence",nanoaod::FlatTable::IntColumn);
+    ppsTab->addColumn<float>("x",trackX,"local track x",nanoaod::FlatTable::FloatColumn,precision_);
+    ppsTab->addColumn<float>("xUnc",trackXUnc,"local track x uncertainty",nanoaod::FlatTable::FloatColumn,precision_);
+    ppsTab->addColumn<float>("y",trackY,"local track y",nanoaod::FlatTable::FloatColumn,precision_);
+    ppsTab->addColumn<float>("yUnc",trackYUnc,"local track y uncertainty",nanoaod::FlatTable::FloatColumn,precision_);
+    ppsTab->addColumn<float>("time",trackTime,"local track time",nanoaod::FlatTable::FloatColumn,precision_);
+    ppsTab->addColumn<float>("timeUnc",trackTimeUnc,"local track time uncertainty",nanoaod::FlatTable::FloatColumn,precision_);
+    ppsTab->addColumn<int>("decRPId",decRPId,"local track detector dec id",nanoaod::FlatTable::IntColumn);
+    ppsTab->addColumn<int>("numFitPoints",numFitPoints,"number of points used for fit",nanoaod::FlatTable::IntColumn);
+    ppsTab->addColumn<int>("pixelRecoInfo",pixelRecoInfo,"flag if a ROC was shifted by a bunchx",nanoaod::FlatTable::IntColumn);
+    ppsTab->addColumn<float>("normalizedChi2",normalizedChi2,"chi2 over NDF",nanoaod::FlatTable::FloatColumn,precision_);
+    ppsTab->addColumn<int>("rpType",rpType,"strip=3, pixel=4, diamond=5, timing=6",nanoaod::FlatTable::IntColumn);
+    ppsTab->addColumn<float>("localSlopeX",localSlopeX,"track horizontal angle",nanoaod::FlatTable::FloatColumn,precision_);
+    ppsTab->addColumn<float>("localSlopeY",localSlopeY,"track vertical angle",nanoaod::FlatTable::FloatColumn,precision_);
+    ppsTab->setDoc("ppsLocalTrack variables");
+    
+    // save output
     iEvent.put(std::move(protonRPIdV), "protonRPId");
     iEvent.put(std::move(singleRP_sector45V), "singleRPsector45");
     iEvent.put(std::move(singleRP_sector56V), "singleRPsector56");
     iEvent.put(std::move(multiRP_sector45V), "multiRPsector45");
     iEvent.put(std::move(multiRP_sector56V), "multiRPsector56");
     iEvent.put(std::move(ppsTab), "ppsTrackTable");
-  } 
+  }
 
   // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
   static void fillDescriptions(edm::ConfigurationDescriptions & descriptions) {
